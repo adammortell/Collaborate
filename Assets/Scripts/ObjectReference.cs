da548fff9f72f538a126c50b98c5bcad4f918ed3
Adam Mortell <amortell@tpctraining.com>
2020-11-02 11:31:03 -0600

    First
    Initial conditions for test before Collaborate is activated.

---
+++ Assets/Scripts/ObjectReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ObjectReference : MonoBehaviour
+{
+    [SerializeField]
+    public string obj_ID = "";
+
+    public ObjectReference() {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SceneManager : MonoBehaviour {
+   
+    private List<string> u_ids;
+    public List<GameObject> objects_;
+
+    void Awake() {
+        u_ids = new List<string>();
+        objects_ = new List<GameObject>();
+
+        for(int i = 0; i < objects_.Count; i++) {
+            string id = objects_[i].GetComponent<ObjectReference>().obj_ID;
+            u_ids.Add(id);
+        }
+
+        for(int i = 0; i < u_ids.Count;i++) {
+            Debug.Log("SCENE OBJECT " + i + " : " + u_ids[i]);
+        }
+    }
+    
+    void  CheckForObjectChange() {
+
+    }
+
+    private void Update()
+    {
+        CheckForObjectChange();
+    }
+
+
+}
